d1f9805a99bd05f1a055563e1cc2c9996b395208
kerwinteh <kerwintehkerwei@gmail.com>
2023-02-15 22:27:04 -0500

    edits

--- main.py
+++ main.py
@@ -3,22 +3,6 @@ import tweepy
 import os
 import random
 
-# session = HTMLSession()
-# url = 'https://clublocker.com/teams/33722/results'
-# r = session.get(url)
-# r.html.render(sleep=3, keep_page=True, scrolldown=1)
-#
-# all_keys = open('twitterBot/keys', 'r',).read().splitlines()
-# api_key = all_keys[0]
-# api_key_secret = all_keys[1]
-# access_token = all_keys[2]
-# access_token_secret = all_keys[3]
-#
-# authenticator = tweepy.OAuthHandler(api_key, api_key_secret)
-# authenticator.set_access_token(access_token, access_token_secret)
-#
-# api = tweepy.API(authenticator, wait_on_rate_limit=True)
-
 session = HTMLSession()
 url = 'https://clublocker.com/teams/33722/results'
 r = session.get(url)
